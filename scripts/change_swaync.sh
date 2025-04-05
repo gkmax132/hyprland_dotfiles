d5b4fd1b7416c5f4de9736ce2c15c3881a04afa1
gkmax132 <gkmax122@gmail.com>
2025-04-05 14:36:37 +0200

    Update change_swaync.sh

--- scripts/change_swaync.sh
+++ scripts/change_swaync.sh
@@ -3,26 +3,21 @@
 THEME_DIR="$HOME/.config/swaync/themes"
 CONFIG_DIR="$HOME/.config/swaync"
 
-SELECTED_THEME=$(ls "$THEME_DIR" | rofi -dmenu --prompt "Выберите тему:")
+SELECTED_THEME=$(ls "$THEME_DIR" | rofi -dmenu --prompt "Select theme:")
 if [[ -z "$SELECTED_THEME" ]]; then
     exit 0
 fi
 
 THEME_PATH="$THEME_DIR/$SELECTED_THEME"
 
-# Убиваем существующий процесс swaync
 pkill -x swaync
 
-# Копируем файлы темы в директорию конфигурации swaync
 cp -r "$THEME_PATH/"* "$CONFIG_DIR/"
 
-# Запускаем swaync в фоновом режиме
 swaync &
 
-# Даем небольшую задержку для запуска
 sleep 0.5
 
-# Перезапуск и обновление настроек swaync
 swaync-client -R && swaync-client -rs
 
 notify-send "Тема: $SELECTED_THEME"
