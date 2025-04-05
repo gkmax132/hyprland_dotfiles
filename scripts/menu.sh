493ba47faf6ac8011a4291d8fb37cc66b1a08ea4
gkmax132 <gkmax122@gmail.com>
2025-04-05 14:35:50 +0200

    Update menu.sh

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CHOICE=$(echo -e "Change wallpaper\nChange waybar theme\nKitty theme selector\nHypr animation selector\nChange border\nToggle blur" | rofi -dmenu -p "Choose an action")
+CHOICE=$(echo -e "Change wallpaper\nChange waybar theme\nKitty theme selector\nHypr animation selector\nChange border\nChange Swaync theme\nToggle blur" | rofi -dmenu -p "Choose an action")
 
 case "$CHOICE" in
     "Change wallpaper") sh $HOME/.scripts/change_wallpaper.sh ;;
@@ -8,7 +8,7 @@ case "$CHOICE" in
     "Kitty theme selector") sh $HOME/.scripts/kitty.sh;;
     "Hypr animation selector") sh $HOME/.scripts/animation.sh;;
     "Change border") sh $HOME/.scripts/change_border.sh;;
+    "Change Swaync theme") sh $HOME/.scripts/change_swaync.sh ;;
     "Toggle blur") sh $HOME/.scripts/toggle_blur.sh ;;
     *) exit 1 ;;
 esac
-
